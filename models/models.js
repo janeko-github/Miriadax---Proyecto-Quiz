2888c2fa6afd7ae7b2bb9e93158a39a9963ce7d2
janeko-github <registro@palarcon.com>
2015-07-04 21:36:03 +0200

    Despliegue DB en Heroku 2

--- models/models.js
+++ models/models.js
@@ -1,6 +1,7 @@
 var path = require('path');
 
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+
 var DB_name	= (url[6] || null);
 var user	= (url[2] || null);
 var pwd		= (url[3] || null);
