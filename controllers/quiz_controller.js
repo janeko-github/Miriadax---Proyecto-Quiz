67e7322871027b491cff60f7827b45c80813408d
janeko-github <registro@palarcon.com>
2015-08-01 00:50:58 +0200

    Despliege Heroku 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -34,7 +34,7 @@ exports.index = function(req,res) {
 
 exports.show = function(req,res) {
 	models.Quiz.findAll(req.params.quizId).then(function(quiz) {
-	    res.render('quizes/show',{quiz: quiz});
+	    res.render('quizes/show',{quiz: req.quiz});
 	})
 
 };
@@ -42,11 +42,12 @@ exports.show = function(req,res) {
 
 exports.answer = function(req,res) {
 	models.Quiz.findAll(req.params.quizId).then(function(quiz) {
-	  if(req.query.respuesta === quiz.respuesta) {
-	    res.render('quizes/answer',{quiz: quiz,respuesta: 'Correcto'});
-	  }else {
-	    res.render('quizes/answer',{quiz: quiz,respuesta: 'Incorrecto'});
-	  }		
+		var resultado = 'Incorrecto';
+	  if(req.query.respuesta === req.quiz.respuesta) {
+	    resultado = 'Correcto';
+	  }
+	    res.render('quizes/answer',{quiz: req.quiz,respuesta: resultado});
+	  		
 	})
 
 };
